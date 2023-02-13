fa47926b81db926ed66654f1d37b844491b66102
Andrey-Mkoyan <and.mkoyan07@gmail.com>
2023-02-13 12:33:35 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,3 @@
+let btn = document.getElementById('btn').onclick = function () {
+    window.location.href = "http://127.0.0.1:5500/pages/cv.html";
+};
---
+++ pages/js/script.js
@@ -0,0 +1,49 @@
+const formBtn1 = document.querySelector("#btn-1")
+const formBtnPrev2 = document.querySelector("#btn-2-prev")
+const formBtnNext2 = document.querySelector("#btn-2-next")
+const formBtn3 = document.querySelector("#btn-3")
+
+formBtn1.addEventListener("click", function (e) {
+    gotoNextForm(formBtn1, formBtnNext2, 1, 2)
+    e.preventDefault()
+})
+
+formBtnNext2.addEventListener("click", function (e) {
+    gotoNextForm(formBtnNext2, formBtn3, 2, 3)
+    e.preventDefault()
+})
+
+formBtnPrev2.addEventListener("click", function (e) {
+    gotoNextForm(formBtnNext2, formBtn1, 2, 1)
+    e.preventDefault()
+})
+
+formBtn3.addEventListener("click", function (e) {
+    document.querySelector(`.step--3`).classList.remove("step-active")
+    document.querySelector(`.step--4`).classList.add("step-active")
+    formBtn3.parentElement.style.display = "none"
+    document.querySelector(".form--message").innerHTML = `
+     <h1 class="form--message-text">Your account is successfully created </h1>
+     `
+    e.preventDefault()
+})
+const gotoNextForm = (prev, next, stepPrev, stepNext) => {
+
+    const prevForm = prev.parentElement
+    const nextForm = next.parentElement
+    const nextStep = document.querySelector(`.step--${stepNext}`)
+    const prevStep = document.querySelector(`.step--${stepPrev}`)
+
+    nextForm.classList.add("form-active")
+    nextForm.classList.add("form-active-animate")
+    prevForm.classList.add("form-inactive")
+
+    prevStep.classList.remove("step-active")
+    nextStep.classList.add("step-active")
+
+    setTimeout(() => {
+        prevForm.classList.remove("form-active")
+        prevForm.classList.remove("form-inactive")
+        nextForm.classList.remove("form-active-animate")
+    }, 1000)
+}
